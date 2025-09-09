9e7bc91fab56de7e41f0f7f3cebe3e09c9c7eb21
onyx144 <nesmeyanovnikita@gmail.com>
2025-09-09 23:58:21 +0300

    update: comment 4

--- components/TicTacToe/litlecomponent/Confeti.tsx
+++ components/TicTacToe/litlecomponent/Confeti.tsx
@@ -5,7 +5,7 @@ interface ConfettiProps {
   isActive: boolean;
   level: number;
 }
-/*Confetti component:*/
+/*Confetti component:   s*/
 
 const Confetti: React.FC<ConfettiProps> = ({ isActive, level }) => {
   if (!isActive) return null;
